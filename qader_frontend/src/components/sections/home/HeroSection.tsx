fd3e0b837df056bdcf19d64257815415134c2f49
Imad Eddine <143887361+imadeddineD@users.noreply.github.com>
2025-05-08 10:51:55 +0200

    Update HeroSection.tsx

--- qader_frontend/src/components/sections/home/HeroSection.tsx
+++ qader_frontend/src/components/sections/home/HeroSection.tsx
@@ -4,9 +4,10 @@ import { Button } from "@/components/ui/button"; // Assuming ui is directly unde
 
 const HeroSection = () => {
   return (
-    <div className="flex justify-center items-center flex-col gap-8 mt-11 w-full p-6">
+    <div className=" bg-[#F9F9FA] dark:bg-[#0B1739] font-body">
+    <div className="flex justify-center items-center flex-col gap-8 w-full p-6 pt-11 container mx-auto px-10">
       {/* Title */}
-      <div className="flex justify-center items-center gap-4 text-center">
+      <div className="flex justify-center items-center gap-4 text-center p-2 rounded-[16px] bg-[#FFF] dark:bg-[#D1DBF9] dark:text-black">
         {" "}
         {/* Added text-center for better mobile */}
         <Image
@@ -16,8 +17,9 @@ const HeroSection = () => {
           height={60}
           className="max-w-full h-auto flex-shrink-0" // Added flex-shrink-0
         />
-        <h1 className="text-5xl font-medium max-lg:text-4xl max-md:text-3xl">
-          عندك <span className="text-[#e78b48]">اختبار قدرات؟</span> ومحتاج
+       
+        <h1 className="lg:text-6xl md:text-5xl font-medium max-lg:text-4xl max-md:text-3xl ">
+          عندك <span className="text-[#e78b48] ">اختبار قدرات؟</span> ومحتاج
           مساعدة!!
         </h1>
       </div>
@@ -28,10 +30,10 @@ const HeroSection = () => {
 </p>
       {/* Action Buttons */}
       <div className="gap-3 flex items-center">
-        <Button variant="outline">
+        <Button variant="outline" className=" border-[2px] hover:border-[2px] font-[700] hover:border-[#074182] dark:hover:border-[#3D93F5] dark:border-[#3D93F5] dark:bg-[#3D93F5] hover:dark:bg-transparent ">
           <span> اشتراك</span>
         </Button>
-        <Button variant="default">
+        <Button variant="default" className="  border-[2px] font-[700] hover:border-[#074182] dark:hover:border-[#3D93F5] dark:border-[#3D93F5] bg-transparent">
           <span>تعرف علينا</span>
         </Button>
       </div>
@@ -44,6 +46,7 @@ const HeroSection = () => {
         height={800} // Adjust height based on aspect ratio if needed
         priority // Add priority as this is likely the LCP image
       />
+    </div>
     </div>
   );
 };
