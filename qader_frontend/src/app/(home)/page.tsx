bc07611fcca5c4c4d2a4d1d282762b47d047d5c6
Imad Eddine <143887361+imadeddineD@users.noreply.github.com>
2025-05-15 20:28:17 +0200

    Update page.tsx

--- qader_frontend/src/app/(home)/page.tsx
+++ qader_frontend/src/app/(home)/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import HeroSection from "@/components/sections/home/HeroSection";
 import AboutSection from "@/components/sections/home/AboutSection";
 import ReviewSection from "@/components/sections/home/ReviewSection";
@@ -6,18 +9,30 @@ import StatisticsSection from "@/components/sections/home/StatisticsSection";
 import CallToActionSection from "@/components/sections/home/CallToActionSection";
 
 export default function Home() {
+  const [myData , setMyData] = useState<any>()
+  useEffect(() => {
+    const fetchHomepageContent = async () => {
+      try {
+        const res = await fetch("https://qader.vip/ar/api/v1/content/homepage/");
+        const data : any = await res.json();
+        setMyData(data)
+        console.log("📦 Homepage API Response:", data);
+      } catch (error) {
+        console.error("❌ Failed to fetch homepage data:", error);
+      }
+    };
+
+    fetchHomepageContent();
+  }, []);
+
   return (
-    <div className="">
-      <HeroSection />
-      <AboutSection />
-      
+    <div>
+      <HeroSection data={myData} />
+      <AboutSection data={myData} />
       <ReviewSection />
-      
-      <AdvantageSection />
-      
-      <StatisticsSection />
-      
-      <CallToActionSection /> 
+      <AdvantageSection data={myData} />
+      <StatisticsSection data={myData} />
+      <CallToActionSection />
     </div>
   );
 }
