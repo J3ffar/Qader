47c8ea28b9a6e403915e9811058ecf3740a9ccdd
Imad Eddine <143887361+imadeddineD@users.noreply.github.com>
2025-05-15 20:42:25 +0200

    Update AdvantageSection.tsx

--- qader_frontend/src/components/sections/home/AdvantageSection.tsx
+++ qader_frontend/src/components/sections/home/AdvantageSection.tsx
@@ -29,9 +29,9 @@ const advantages = [
   },
 ];
 
-const AdvantageSection = () => {
+const AdvantageSection = ({data} : any) => {
   return (
-    <div className=" bg-[#FDFDFD] dark:bg-[#081028]">
+    <div className=" bg-white dark:bg-[#081028] sm:px-0 px-4">
     <div className="h-full flex justify-center items-center max-md:flex-col-reverse py-6 container mx-auto px-0 gap-9">
       {/* Text Content Section */}
       <div className="w-full flex-1/2">
@@ -44,17 +44,31 @@ const AdvantageSection = () => {
           ما الذى يجعلنا نتميز عن المنافسين.
         </p>
         <div className="flex flex-col gap-3 mt-6">
-          {advantages.map((advantage) => (
-            <p
-              key={advantage.id}
-              className={`p-4 rounded-3xl transition delay-150 duration-300 ease-in-out font-heading dark:bg-[#0B1739] bg-[#E7F1FE] hover:dark:bg-[#053061]
-                 hover:bg-[#074182] hover:text-[#FDFDFD]   hover:dark:font-[600] hover:dark:text-[#FDFDFD] text-[22px]
-              `}
-            >
-              {`${advantage.id} ${advantage.text}`} {/* Use template literal */}
-            </p>
-          ))}
-        </div>
+  {data?.features && data.features.length > 0 ? (
+    data.features.map((feature : any, index : any) => (
+      <p
+        key={index}
+        className={`py-2 px-4 rounded-[16px] transition delay-150 duration-300 ease-in-out font-heading dark:bg-[#0B1739] bg-[#E7F1FE] hover:dark:bg-[#053061]
+          hover:bg-[#074182] hover:text-[#FDFDFD] hover:dark:font-[600] hover:dark:text-[#FDFDFD] text-[22px]
+        `}
+      >
+        {feature.title} - {feature.text}
+      </p>
+    ))
+  ) : (
+    advantages.map((advantage) => (
+      <p
+        key={advantage.id}
+        className={`py-2 px-4 rounded-[16px] transition delay-150 duration-300 ease-in-out font-heading dark:bg-[#0B1739] bg-[#E7F1FE] hover:dark:bg-[#053061]
+          hover:bg-[#074182] hover:text-[#FDFDFD] hover:dark:font-[600] hover:dark:text-[#FDFDFD] text-[22px]
+        `}
+      >
+        {`${advantage.id}  ${advantage.text}`}
+      </p>
+    ))
+  )}
+</div>
+
       </div>
       {/* Image Section */}
       <div className="flex flex-1/2">
